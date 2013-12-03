b0de172b6f1511219d33d2df396eeec70e1f789d
Manoj <pmontu@gmail.com>
2013-12-03 09:14:03 +0530

    increments

--- test3.py
+++ test3.py
@@ -9,18 +9,27 @@ a+=[j]
 s=0
 i+=1
 
-print a[len(a)-2],a[len(a)-1]
+k=a[len(a)-2]
+
+print k,a[len(a)-2],a[len(a)-1],a[len(a)-2]<a[len(a)-1]
 
 while(i<=evensize/2):
-    if s==1:
-        a+=[j]
-        print a[len(a)-2],a[len(a)-1]
-        s=0
-        i+=1
+
+    if a[len(a)-2]<a[len(a)-1]:
+        k+=1
     else:
-        a+= [i]
-        print a[len(a)-2],a[len(a)-1]
-        s=1
-        j-=1
-    if (i==j):
-        break
+        k-=1
+
+    print k,a[len(a)-2],a[len(a)-1],a[len(a)-2]<a[len(a)-1]
+
+    if k == a[len(a)-1]:
+        if s==1:
+            a+=[j]
+            s=0
+            i+=1
+        else:
+            a+= [i]
+            s=1
+            j-=1
+        if (i==j):
+            break
