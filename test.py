41771ce11c2d2b7c0e26998dcb63ec21d2965495
Manoj <pmontu@gmail.com>
2013-12-03 06:53:34 +0530

    first of 6 = 0 6 1 5 2 4 3

--- test.py
+++ test.py
@@ -1,20 +1,18 @@
-evensize = 10
-a = []
+evensize = 6
 for k in range(1):
 	i=0;
 	j=evensize;
+	a = [i]
+	s = 1
 	while(i<=evensize/2):
-		a += [i,j]
-		i+=1
-		j-=1
-		if (i==j):
+		if s==1:
+			a+=[j]
+			s=0
+			i+=1
+		else:
+			a+= [i]
+			s=1
 			j-=1
-			while(i<=evensize):
-				if j==-1:
-					a += [i]
-					break
-				a += [i,j]
-				i+=1
-				j-=1
+		if (i==j):
 			break
 print a
