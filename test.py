ebfc3986e16b26841811b66df6da0e2cf8dbf191
Manoj <pmontu@gmail.com>
2013-12-03 14:42:59 +0530

    scratch solid scratch

--- test.py
+++ test.py
@@ -1,18 +1,58 @@
-evensize = 6
-for k in range(1):
-	i=0;
-	j=evensize;
-	a = [i]
-	s = 1
-	while(i<=evensize/2):
-		if s==1:
-			a+=[j]
-			s=0
-			i+=1
-		else:
-			a+= [i]
-			s=1
-			j-=1
-		if (i==j):
-			break
-print a
+evenvibrations = 6
+
+i=0
+a = [i]
+s = 1
+j=evenvibrations;
+
+a+=[j]
+s=0
+i+=1
+
+k=a[len(a)-2]
+
+print k,a[len(a)-2],a[len(a)-1],a[len(a)-2]<a[len(a)-1]
+
+for z in range(100):
+
+    if a[len(a)-2]<a[len(a)-1]:
+        k+=1
+    else:
+        k-=1
+
+    print k,a[len(a)-2],a[len(a)-1],a[len(a)-2]<a[len(a)-1]
+
+    if k == a[len(a)-1]:
+        if s==1:
+            a+=[j]
+            s=0
+            i+=1
+        else:
+            a+= [i]
+            s=1
+            j-=1
+            if i==6:
+                print "Top"
+
+                i=0
+                a = [i]
+                s = 1
+                j=evenvibrations;
+
+                a+=[j]
+                s=0
+                i+=1
+
+                k=a[len(a)-2]
+
+                print k,a[len(a)-2],a[len(a)-1],a[len(a)-2]<a[len(a)-1]
+
+        if (i==j):
+            s = 1
+            j=i-1
+            a+=[j]
+            k-=1
+            print k,a[len(a)-2],a[len(a)-1],a[len(a)-2]<a[len(a)-1]
+            print "Mid"
+            s=0
+            i+=1
